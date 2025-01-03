4cdb760a5edeabc9267bb8e6b57293f57d67fb1f
Набиев Эльтадж <46077043+elikman@users.noreply.github.com>
2025-01-03 23:16:25 +0300

    Update charity_crud.py

--- app/crud/charity_crud.py
+++ app/crud/charity_crud.py
@@ -3,8 +3,13 @@ from sqlalchemy.exc import IntegrityError
 from app.models import CharityProject
 from app.schemas.charity_project import CharityProjectCreate, CharityProjectUpdate
 
+
 class CharityCrud:
-    async def create(self, obj_in: CharityProjectCreate, session: AsyncSession) -> CharityProject:
+    async def create(
+        self,
+        obj_in: CharityProjectCreate,
+        session: AsyncSession
+    ) -> CharityProject:
         new_obj = CharityProject(**obj_in.dict())
         session.add(new_obj)
         try:
@@ -15,8 +20,14 @@ class CharityCrud:
             raise e
         return new_obj
 
-    async def patch(self, db_obj: CharityProject, obj_in: CharityProjectUpdate, session: AsyncSession) -> CharityProject:
-        for field, value in obj_in.dict(exclude_unset=True).items():
+    async def patch(
+        self,
+        db_obj: CharityProject,
+        obj_in: CharityProjectUpdate,
+        session: AsyncSession
+    ) -> CharityProject:
+        for field, value in obj_in.dict(
+            exclude_unset=True).items():
             setattr(db_obj, field, value)
         try:
             await session.commit()
@@ -26,7 +37,11 @@ class CharityCrud:
             raise e
         return db_obj
 
-    async def delete(self, db_obj: CharityProject, session: AsyncSession) -> CharityProject:
+    async def delete(
+        self,
+        db_obj: CharityProject,
+        session: AsyncSession
+    ) -> CharityProject:
         await session.delete(db_obj)
         try:
             await session.commit()
