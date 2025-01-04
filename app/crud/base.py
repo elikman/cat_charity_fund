cdb02a961743a12a83c6d9052ac45180c850cafc
Набиев Эльтадж <46077043+elikman@users.noreply.github.com>
2025-01-05 00:39:58 +0300

    Update base.py

--- app/crud/base.py
+++ app/crud/base.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import Base
 
-ModelType = TypeVar("ModelType", bound=Base) 
+ModelType = TypeVar("ModelType", bound=Base)
 CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
 UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
 
