95d0c93b67b626c32b6e0995781d4be6432fb7e0
Эльтадж <elikmang.o.e@yandex.ru>
2025-01-04 17:07:56 +0300

    pep8

--- app/crud/base.py
+++ app/crud/base.py
@@ -1,52 +1,52 @@
-from datetime import datetime 
-from typing import TypeVar, Optional, Generic 
- 
-from pydantic import BaseModel 
-from sqlalchemy import select, asc 
-from sqlalchemy.ext.asyncio import AsyncSession 
- 
-from app.core.db import Base 
- 
-ModelType = TypeVar("ModelType", bound=Base) 
-CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel) 
-UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel) 
- 
- 
-class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]): 
-    def __init__(self, model): 
-        self.model = model 
- 
-    async def get_object( 
-        self, object_id: int, session: AsyncSession 
-    ) -> Optional[ModelType]: 
-        return ( 
-            ( 
-                await session.execute( 
-                    select(self.model).where(self.model.id == object_id) 
-                ) 
-            ) 
-            .scalars() 
-            .first() 
-        ) 
- 
-    async def get_all_objects(self, session: AsyncSession) -> list[ModelType]: 
-        return (await session.execute(select(self.model))).scalars().all() 
- 
-    async def get_all_objects_is_unclosed( 
-        self, session: AsyncSession 
-    ) -> list[ModelType]: 
-        return ( 
-            ( 
-                await session.execute( 
-                    select(self.model) 
-                    .where(self.model.fully_invested == 0) 
-                    .order_by(asc("create_date")) 
-                ) 
-            ) 
-            .scalars() 
-            .all() 
-        ) 
- 
+from datetime import datetime
+from typing import TypeVar, Optional, Generic
+
+from pydantic import BaseModel
+from sqlalchemy import select, asc
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.db import Base
+
+ModelType = TypeVar("ModelType", bound=Base)
+CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
+UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
+
+
+class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+    def __init__(self, model):
+        self.model = model
+
+    async def get_object(
+        self, object_id: int, session: AsyncSession
+    ) -> Optional[ModelType]:
+        return (
+            (
+                await session.execute(
+                    select(self.model).where(self.model.id == object_id)
+                )
+            )
+            .scalars()
+            .first()
+        )
+
+    async def get_all_objects(self, session: AsyncSession) -> list[ModelType]:
+        return (await session.execute(select(self.model))).scalars().all()
+
+    async def get_all_objects_is_unclosed(
+        self, session: AsyncSession
+    ) -> list[ModelType]:
+        return (
+            (
+                await session.execute(
+                    select(self.model)
+                    .where(self.model.fully_invested == 0)
+                    .order_by(asc("create_date"))
+                )
+            )
+            .scalars()
+            .all()
+        )
+
     async def create(
         self,
         obj_in: CreateSchemaType,
@@ -61,23 +61,23 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         await session.commit()
         await session.refresh(created_obj)
         return created_obj
- 
-    @staticmethod 
-    async def delete(db_object: ModelType, session: AsyncSession) -> ModelType: 
-        await session.delete(db_object) 
-        await session.commit() 
-        return db_object 
- 
-    @classmethod 
-    async def patch( 
-        cls, db_obj: ModelType, obj_in: UpdateSchemaType, session: AsyncSession 
-    ) -> ModelType: 
-        update_data = obj_in.dict(exclude_unset=True) 
-        for field in update_data: 
-            setattr(db_obj, field, update_data[field]) 
-        if db_obj.full_amount == db_obj.invested_amount: 
-            db_obj.fully_invested = True 
-            db_obj.close_date = datetime.now() 
-            await session.commit() 
-            await session.refresh(db_obj) 
-        return db_obj 
+
+    @staticmethod
+    async def delete(db_object: ModelType, session: AsyncSession) -> ModelType:
+        await session.delete(db_object)
+        await session.commit()
+        return db_object
+
+    @classmethod
+    async def patch(
+        cls, db_obj: ModelType, obj_in: UpdateSchemaType, session: AsyncSession
+    ) -> ModelType:
+        update_data = obj_in.dict(exclude_unset=True)
+        for field in update_data:
+            setattr(db_obj, field, update_data[field])
+        if db_obj.full_amount == db_obj.invested_amount:
+            db_obj.fully_invested = True
+            db_obj.close_date = datetime.now()
+            await session.commit()
+            await session.refresh(db_obj)
+        return db_obj
