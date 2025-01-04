f87b551681db57f06d52ccaa827d471bc2b1e585
Набиев Эльтадж <46077043+elikman@users.noreply.github.com>
2025-01-04 16:36:12 +0300

    Update charity_project.py

--- app/api/endpoints/charity_project.py
+++ app/api/endpoints/charity_project.py
@@ -31,29 +31,26 @@ async def get_all_charity_projects(
     return await charity_crud.get_all_objects(session) 
  
  
-@router.post( 
-    "/", 
-    dependencies=[Depends(current_superuser)], 
-    response_model=CharityProjectDB, 
-    response_model_exclude_none=True, 
-) 
-async def create_charity_project( 
-    charity_project: CharityProjectCreate = Body( 
-        example=CharityProjectCreate.Config.schema_extra["example"] 
-    ), 
-    session: AsyncSession = Depends(get_async_session), 
-) -> CharityProject: 
-    await check_name(charity_project.name, session) 
-    new_charity_project = await charity_crud.create(charity_project, session) 
-    unclosed_donations = await donation_crud.get_all_objects_is_unclosed( 
-        session 
-    ) 
-    if unclosed_donations: 
-        invested = make_investments(new_charity_project, unclosed_donations) 
-        session.add_all(invested) 
-    await session.commit() 
-    await session.refresh(new_charity_project) 
-    return new_charity_project 
+@router.post(
+    "/",
+    dependencies=[Depends(current_superuser)],
+    response_model=CharityProjectDB,
+    response_model_exclude_none=True,
+)
+async def create_charity_project(
+    charity_project: CharityProjectCreate = Body(
+        example=CharityProjectCreate.Config.schema_extra["example"]
+    ),
+    session: AsyncSession = Depends(get_async_session),
+) -> CharityProject:
+    await check_name(charity_project.name, session)
+    new_charity_project = await charity_crud.create(charity_project, session)
+    unclosed_donations = await donation_crud.get_all_objects_is_unclosed(session)
+    if unclosed_donations:
+        invested = make_investments(new_charity_project, unclosed_donations)
+        session.add_all(invested)
+        await session.commit()
+    return new_charity_project
  
  
 @router.patch( 
