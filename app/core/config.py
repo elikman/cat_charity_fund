5e88f5dfb954adb71c553771c5786f950096114c
Набиев Эльтадж <46077043+elikman@users.noreply.github.com>
2025-01-09 19:27:12 +0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,6 @@
 from typing import Optional
 from dotenv import load_dotenv
 from pydantic import EmailStr, BaseSettings
-#from pydantic_settings import BaseSettings
-
-load_dotenv()
 
 
 class Settings(BaseSettings):
