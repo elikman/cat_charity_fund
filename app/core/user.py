dcae8d8623a0bcf1616b0e2e96e3df6911dd1f2b
Набиев Эльтадж <46077043+elikman@users.noreply.github.com>
2025-01-09 22:09:53 +0300

    Update user.py

--- app/core/user.py
+++ app/core/user.py
@@ -2,9 +2,9 @@ from typing import Optional, Union
 import logging
 
 from fastapi import Depends, Request
-from fastapi_users import (BaseUserManager, FastAPIUsers, IntegerIDMixin, 
+from fastapi_users import (BaseUserManager, FastAPIUsers, IntegerIDMixin,
                            InvalidPasswordException)
-from fastapi_users.authentication import (AuthenticationBackend, 
+from fastapi_users.authentication import (AuthenticationBackend,
                                           BearerTransport, JWTStrategy)
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -20,19 +20,24 @@ logger = logging.getLogger(__name__)
 MIN_PASSWORD_LENGTH = 3
 JWT_LIFETIME_SECONDS = 3600
 
+
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
     yield SQLAlchemyUserDatabase(session, User)
 
 bearer_transport = BearerTransport(tokenUrl='auth/jwt/login')
 
+
 def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=settings.secret, lifetime_seconds=JWT_LIFETIME_SECONDS)
+    return JWTStrategy(secret=settings.secret,
+                       lifetime_seconds=JWT_LIFETIME_SECONDS)
+
 
 auth_backend = AuthenticationBackend(
     name='jwt',
     transport=bearer_transport,
     get_strategy=get_jwt_strategy,
 )
+
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
 
