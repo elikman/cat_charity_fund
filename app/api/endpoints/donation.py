a2be1ba4c602b591f35d22aea2d39bda8137ebea
Набиев Эльтадж <46077043+elikman@users.noreply.github.com>
2025-01-09 16:42:50 +0300

    Update donation.py

--- app/api/endpoints/donation.py
+++ app/api/endpoints/donation.py
@@ -15,41 +15,43 @@ router = APIRouter()
              response_model=DonationCreate,
              response_model_exclude_none=True
              )
-async def create_donation(donation: DonationBase,
-                          session: AsyncSession = Depends(get_async_session),
-                          user: User = Depends(current_user)
-                          ):
+async def create_donation(
+    donation: DonationBase,
+    session: AsyncSession = Depends(get_async_session),
+    user: User = Depends(current_user)
+):
     """Сделать пожертвование."""
     new_donation = await donation_crud.create(donation, session, user)
     await investing_process(new_donation, CharityProject, session)
-
     return new_donation
 
 
-@router.get( 
-    '/my', 
-    response_model=list[DonationCreate], 
-    response_model_exclude={'user_id'}, 
-) 
-async def get_user_donations( 
-    session: AsyncSession = Depends(get_async_session), 
-    user: User = Depends(current_user) 
-): 
-    """Вернуть список пожертвований пользователя, выполняющего запрос.""" 
-    return await donation_crud.get_by_user(session=session, user=user)
+@router.get(
+    '/my',
+    response_model=list[DonationCreate],
+    response_model_exclude={'user_id'},
+)
+async def get_user_donations(
+    session: AsyncSession = Depends(get_async_session),
+    user: User = Depends(current_user)
+):
+    """Вернуть список пожертвований пользователя, выполняющего запрос."""
+    return await donation_crud.get_by_user(
+        session=session, user=user
+    )
 
 
-@router.get( 
-    '/', 
-    response_model=list[DonationDB], 
-    response_model_exclude_none=True, 
-    dependencies=[Depends(current_superuser)], 
-) 
-async def get_all_donations( 
-    session: AsyncSession = Depends(get_async_session), 
-): 
-    """Только для суперюзеров. 
- 
-    Возвращает список всех пожертвований. 
-    """ 
+@router.get(
+    '/',
+    response_model=list[DonationDB],
+    response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)],
+)
+async def get_all_donations(
+    session: AsyncSession = Depends(get_async_session),
+):
+    """Только для суперюзеров.
+
+    Возвращает список всех пожертвований.
+    """
     return await donation_crud.get_multi(session)
